0407c6a39bd2ae47607d299790f4b4a396258259
gabriella kovacs <kovacsxgabriella@gmail.com>
2015-11-03 16:56:29 +0000

    initial setup

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+// Gulpfile.js
+
+var gulp = require("gulp"),
+    uglify = require("gulp-uglify"),
+    concat = require("gulp-concat"),
+    sass = require('gulp-sass');
+ 
+
+ //compile sass files
+gulp.task('sass', function () {
+  gulp.src('./sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+});
+ 
+
+//compile sass files on the go
+gulp.task('sass:watch', function () {
+  gulp.watch('./sass/**/*.scss', ['sass']);
+});
+
+
+//concatenate and minify css files
+gulp.task("minify", function() {
+  return gulp.src("./src/css/*.css")
+    .pipe(concat("all.css"))
+    .pipe(uglify())
+    .pipe(gulp.dest("./dist/"));
+});
